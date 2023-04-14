47aeee571497eac1fec5b1cc3cec669fcbfa260d
GuiJKS <guilherme_junkes@estudante.sesisenai.org.br>
2023-04-14 08:13:21 -0300

    sla

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>PAGINA DO HOME</h1>
+    <h1>PAGINA DO HOME</h1>
+    <p>No nosso site você pode encontrar desde suplementos de academia até ferramentas para melhorar seus treinos</p>
     </div>
   );
 }
